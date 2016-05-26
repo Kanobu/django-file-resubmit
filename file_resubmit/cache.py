e80c4bc24ce02219e1ae26145629e7f7b2103589
Yuri Egorov <ysegorov@gmail.com>
2016-05-26 22:15:05 +0300

    save file to storage instead of cache

--- file_resubmit/cache.py
+++ file_resubmit/cache.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-try:
-    from cStringIO import StringIO as BytesIO
-except ImportError:
-    from io import BytesIO
+
+import os
 
 # Django 1.9 removes support for django.core.cache.get_cache
 try:
@@ -11,6 +9,7 @@ except ImportError:
     from django.core.cache import caches
     get_cache = lambda cache_name: caches[cache_name]
 
+from django.core.files.storage import default_storage as storage
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
@@ -21,13 +20,26 @@ class FileCache(object):
     def get_backend(self):
         return get_cache('file_resubmit')
 
+    def save_to_storage(self, key, upload):
+        return storage.save(os.path.join('file_resubmit', key), upload.file)
+
+    def get_from_storage(self, name):
+        if storage.exists(name):
+            return storage.open(name, 'rb')
+
+    def delete_from_storage(self, name):
+        if storage.exists(name):
+            return storage.delete(name)
+
     def set(self, key, upload):
+        storage_name = self.save_to_storage(key, upload)
         state = {
             "name": upload.name,
             "size": upload.size,
             "content_type": upload.content_type,
             "charset": upload.charset,
-            "content": upload.file.read()}
+            "storage_name": storage_name,
+        }
         upload.file.seek(0)
         self.backend.set(key, state)
 
@@ -35,8 +47,9 @@ class FileCache(object):
         upload = None
         state = self.backend.get(key)
         if state:
-            f = BytesIO()
-            f.write(state["content"])
+            f = self.get_from_storage(state["storage_name"])
+            if not f:
+                return upload
             upload = InMemoryUploadedFile(
                 file=f,
                 field_name=field_name,
@@ -49,4 +62,7 @@ class FileCache(object):
         return upload
 
     def delete(self, key):
+        state = self.backend.get(key)
+        if state:
+            self.delete_from_storage(state["storage_name"])
         self.backend.delete(key)
